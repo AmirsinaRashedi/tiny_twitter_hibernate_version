dbd70d43051e4b92e63a7d516e396708d0289904
AmirsinaRashedi <amirsina.rashedi@gmail.com>
2022-07-17 18:16:50 +0430

    defined base domain

---
+++ src/main/java/base/domain/BaseEntity.java
@@ -0,0 +1,28 @@
+package base.domain;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+
+@MappedSuperclass
+public class BaseEntity<ID extends Serializable> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private ID id;
+
+    public BaseEntity() {
+    }
+
+    public ID getId() {
+        return id;
+    }
+
+    public void setId(ID id) {
+        this.id = id;
+    }
+
+}
