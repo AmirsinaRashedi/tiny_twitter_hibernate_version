7946ca221461aeaf0ddb575a1c463ccd5bff26e8
AmirsinaRashedi <amirsina.rashedi@gmail.com>
2022-07-19 17:18:15 +0430

    defined Menu

---
+++ src/main/java/util/Menu.java
@@ -0,0 +1,13 @@
+package util;
+
+public class Menu {
+
+    public static void deleteCommentMenu() {
+
+        System.out.println("1- delete comment from your tweets");
+        System.out.println("2- delete your comment");
+
+    }
+
+
+}
